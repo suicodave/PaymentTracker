9125bdb847a42732a8a481e6f832d7fb865791e7
suico.dave@gmail.com <suico.dave@gmail.com>
2022-11-13 17:50:22 +0800

    Add Payments page to demonstrate left joins

--- PaymentTracker/Controllers/PaymentsController.cs
+++ PaymentTracker/Controllers/PaymentsController.cs
@@ -19,6 +19,25 @@ namespace PaymentTracker.Controllers
             this.connection = new SqlConnection(connectionString);
         }
 
+        public async Task<ActionResult> Index()
+        {
+            string query = @"select 
+            p.Id,
+            p.Amount,
+            p.EffectiveDate,
+            CONCAT(c.FirstName,' ',c.LastName) CustomerName
+
+            from Payments p
+
+            left join Customers c on p.CustomerId = c.Id";
+
+            IEnumerable<PaymentWithCustomerName> payments = await connection.QueryAsync<PaymentWithCustomerName>(query);
+
+            ViewData["payments"] = payments;
+
+            return View();
+        }
+
         public async Task<ActionResult> Create(Payment payment)
         {
             string query = "insert into Payments values(@Amount,@EffectiveDate,@CustomerId)";
---
+++ PaymentTracker/Models/Payments/PaymentWithCustomerName.cs
@@ -0,0 +1,13 @@
+﻿namespace PaymentTracker.Models.Payments
+{
+    public class PaymentWithCustomerName
+    {
+        public long Id { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public DateTime EffectiveDate { get; set; }
+
+        public string CustomerName { get; set; } = string.Empty;
+    }
+}
